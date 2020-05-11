83f3318e4d8dd8031ad3884e0c32e68c87314cdb
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-11 22:20:15 +0900

    Add Employee commit

--- magazine.go
+++ magazine.go
@@ -1,6 +1,12 @@
 package magazine
+
 type Subscriber struct {
-	Name string
-	Rate float64
+	Name   string
+	Rate   float64
 	Active bool
+}
+
+type Employee struct {
+	Name   string
+	Salary float64
 }
