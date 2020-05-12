af20b1c96ec48248ae42dbfafcbe5e44b9991a2d
Zetta Kim <zettabyte.kim@gmail.com>
2020-05-12 21:36:29 +0900

    5/12 commit

--- magazine.go
+++ magazine.go
@@ -1,16 +1,19 @@
 package magazine
 
 type Subscriber struct {
-	Name        string
-	Rate        float64
-	Active      bool
-	HomeAddress Address
+	Name   string
+	Rate   float64
+	Active bool
+	// HomeAddress Address
+	// 익명필드(anonymous field)로 변경
+	Address
 }
 
 type Employee struct {
-	Name        string
-	Salary      float64
-	HomeAddress Address
+	Name   string
+	Salary float64
+	// HomeAddress Address
+	Address
 }
 
 type Address struct {
